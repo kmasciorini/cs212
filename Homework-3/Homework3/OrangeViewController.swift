48a037fe3958e4f669f0bf317d296157c045524c
kmasciorini <kmash08@gmail.com>
2016-02-08 20:57:13 -0800

    wrote code for homework 3

--- Homework-3/Homework3/OrangeViewController.swift
+++ Homework-3/Homework3/OrangeViewController.swift
@@ -9,5 +9,34 @@
 import UIKit
 
 class OrangeViewController : UIViewController {
+    
+    var count = 0
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear (animated)
         
+        print("Orange. viewWillAppear.")
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear (animated)
+        
+        print("Orange. viewDidAppear.")
+        count++
+        print("the orange viewDidAppear count is \(count)")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear (animated)
+        
+        print("Orange. viewWillDisappear.")
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear (animated)
+        
+        print("Orange. viewDidDisappear.")
+    }
+    
 }
